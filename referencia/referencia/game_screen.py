0cf869f9bf0038a0f12d241d0c03c41fc00ca843
Guidolscr <81170424+Guidolscr@users.noreply.github.com>
2021-08-09 13:44:45 -0300

    commit

--- referencia/referencia/game_screen.py
+++ referencia/referencia/game_screen.py
@@ -57,19 +57,27 @@ def game_screen(window):
                     # Dependendo da tecla, altera a velocidade.
                     keys_down[event.key] = True
                     if event.key == pygame.K_LEFT:
-                        player.speedx -= 8
+                        player.speedx -= 4
                     if event.key == pygame.K_RIGHT:
-                        player.speedx += 8
+                        player.speedx += 4
                     if event.key == pygame.K_SPACE:
                         player.shoot()
+                    if event.key == pygame.K_UP:
+                        player.speedy -= 4
+                    if event.key == pygame.K_DOWN:
+                        player.speedy += 4
                 # Verifica se soltou alguma tecla.
                 if event.type == pygame.KEYUP:
                     # Dependendo da tecla, altera a velocidade.
                     if event.key in keys_down and keys_down[event.key]:
                         if event.key == pygame.K_LEFT:
-                            player.speedx += 8
+                            player.speedx += 4
                         if event.key == pygame.K_RIGHT:
-                            player.speedx -= 8
+                            player.speedx -= 4
+                        if event.key == pygame.K_UP:
+                            player.speedy += 4
+                        if event.key == pygame.K_DOWN:
+                            player.speedy -= 4
 
         # ----- Atualiza estado do jogo
         # Atualizando a posição dos meteoros
